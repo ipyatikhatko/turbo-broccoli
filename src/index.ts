733307b13b61791d38258a7c2d1316260f7f9878
ipyatikhatko <elijah.pyatikhatko@gmail.com>
2026-04-07 16:46:41 +0300

    chore: Initialize project with Fastify setup, OpenAPI specification, and basic server implementation.

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import Fastify from "fastify";
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+await fastify.register(swagger, {
+  mode: "static",
+  specification: {
+    baseDir: process.cwd(),
+    path: "openapi.yaml",
+  },
+});
+await fastify.register(swaggerUi, {
+  routePrefix: "/docs",
+});
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({
+      port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
